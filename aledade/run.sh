84af05811662df8527065ec2b42fd9882a959b54
Clark Ritchie <clarkritchie@users.noreply.github.com>
2024-06-02 16:24:03 -0700

    Update run.sh

--- aledade/run.sh
+++ aledade/run.sh
@@ -1,8 +1,7 @@
-docker run -it -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_PASSWORD=password postgres
+#!/usr/bin/env bash
 
-	
+# docker run -it -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_PASSWORD=password postgres
 docker run --rm --name postgres -v .:/data -e POSTGRES_PASSWORD=password -d postgres
 
-docker exec -it -e PGUSER=postgres postgres bash
-
-psql < /data/insert.sql
+# you can run the test SQL from /data in another shell, e.g.
+# docker exec -it -e PGUSER=postgres postgres bash

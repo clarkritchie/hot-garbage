672300505601c99baae933ac94131a6b75a4ccff
Clark Ritchie <clarkritchie@gmail.com>
2024-06-03 09:32:22 -0700

    messing around

--- keebo.ai/entrypoint.sh
+++ keebo.ai/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-/src/app.py
+# /app/app.py
+
+# echo "starting uvicorn on port ${PORT}"
+# uvicorn main:app --port ${PORT} --reload
+
+echo "starting app"
+python3 main.py
+
+# echo "starting server on port ${PORT}"
+# fastapi dev main.py --port ${PORT}

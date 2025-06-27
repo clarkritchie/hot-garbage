4d8ca8224b7bb98031386185d6e33a1fa2226f23
Clark Ritchie <clark.ritchie@dexcom.com>
2025-06-27 14:04:46 -0700

    This is work in progress :safety_vest:

--- bin/safe-branch-prune.zsh
+++ bin/safe-branch-prune.zsh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
+#
+# Removes local branches that are no longer on the remote
+#
+
 git fetch --prune
 
 for branch in ${(f)"$(git for-each-ref --format '%(refname:short) %(upstream:track)' refs/heads | grep '\[gone\]' | awk '{print $1}')"}; do

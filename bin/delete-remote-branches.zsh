e18a7923728835e3e6f5df5142060b457f2a8384
Clark Ritchie <clark.ritchie@dexcom.com>
2025-12-12 10:23:30 -0800

    This is work in progress :safety_vest:

--- bin/delete-remote-branches.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-for branch in $(git branch -r | grep job); do
-  # Remove the 'origin/' prefix
-  clean_branch=${branch#origin/}
-  echo $clean_branch
-  git push origin --delete $clean_branch
-done
